c0b060ad4e96fff276e6831543cfe65a387fb61a
Hellamb <vlad.kuksiuk@gmail.com>
2021-02-09 15:32:17 +0200

    first-commit

---
+++ ЛР1/src/LinkedList.java
@@ -0,0 +1,12 @@
+public class LinkedList {
+    private static class Node{
+        Object element;
+        Node next;
+        Node prev;
+
+        public Node(Object element)
+        {
+            this.element = element;
+        }
+    }
+}
