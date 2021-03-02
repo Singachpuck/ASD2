d5b026e788040e3da957406252f810bcd57e6ae0
Singachpuck <singachpucktv@gmail.com>
2021-03-03 01:35:06 +0200

    Added functionality Lab 2

--- ЛР2/src/Node.java
+++ ЛР2/src/Node.java
@@ -1,11 +1,20 @@
 public class Node<T> {
-    T value;
-    Node<T> left;
-    Node<T> right;
-    Node<T> parent;
+    private T value;
+    Node<T> left = null;
+    Node<T> right = null;
+    Node<T> parent = null;
+    boolean processed = false;
 
     Node(T value)
     {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
         this.value = value;
     }
 }
